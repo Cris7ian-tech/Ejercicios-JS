0497d29bea3ccbcb4f3f28f005c7af00948b68d2
Cris7ian-tech <cristianlucero.ala2@gmail.com>
2024-07-03 03:39:54 -0300

    Ejercicios JavaScript_1

---
+++ app.js
@@ -0,0 +1,69 @@
+
+
+//#1
+// let uno = 7;
+
+// let dos = 7;
+
+// uno = 6;
+
+//#2
+
+// let nombre = "Cristian";
+// console.log(nombre)
+// let edad = 43;
+// console.log(edad)
+// let cumpleaños = "25/10/80";
+// console.log(cumpleaños)
+// let ciudad = "General La Madrid";
+// console.log(ciudad)
+// let ocupacion = "tutor";
+// console.log(ocupacion)
+// let pasatiempos = "el bootcamp";
+// console.log(pasatiempos)
+
+// let nombre= "Cristian";
+// let edad = "43";
+// let cumpleaños = "25/10/80";
+// let ciudad = "General La Madrid";
+// let ocupacion = "tutor";
+// let pasatiempo = "el bootcamp";
+
+
+// console.log(`Hola, me llamo ${nombre}, tengo ${edad} y los cumplo el: ${cumpleaños}, soy de la ciudad de ${ciudad}, soy ${ocupacion} y paso mis dias con el ${pasatiempo}!!!`);
+
+// let nombre = "Cristian",
+//     edad = "43",
+//     cumpleaños = "25/10/80",
+//     ciudad = "General La Madrid",
+//     ocupacion = "tutor",
+//     pasatiempo = "el bootcamp";
+
+
+// #3
+
+// let mensaje = prompt("Ingresa una frase de tu cancion preferida");
+// console.log(mensaje.length(mensaje))
+
+//#4
+
+// let num1 = parseFloat(prompt("Ingresa un numero: "));
+// let num2 = parseFloat(prompt("Ahora, ingresa otro numero: "));
+// let resultado = num1 + num2;
+
+// console.log(`La suma de los valores ingresados es: ${resultado}`);
+
+// #5
+
+// let estadia = parseInt(prompt("Ingresa la cantidad de dias que vas a estar de viaje: "));
+// let cashMax = parseInt(prompt("Ingresa la cantidad de dinero con la que dispones para dicho viaje: "));
+// let comidaTotal = parseInt(prompt("Cuantas comidas piensas hacer en toda tu estadia? recuerda que lo recomendado serian unas 24: "));
+
+// let porComida = cashMax / comidaTotal;
+
+// alert(`Podes gastar ${porComida} en cada comida para que te alcance la plata durante los ${estadia} dias de viaje.`);
+
+
+
+
+
